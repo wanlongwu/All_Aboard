db15afd653cfea19c0097f1f1c6e6b223c09cdf9
beomkyu Chung <nosenal27@gmail.com>
2018-02-27 15:04:40 +0900

    work in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-name
-price / hour
-owner
-boat type
-capacity
-year :1954
-power :130-150
-location
-
-boat.create(name: 'Falcon', price: 600, owner: 'Henry', capacity: 3, year: 1954, power: 130, location: 'Meguro')
-
-boat.create(name: 'Eagle', price: 2100, owner: 'Wan', capacity: 4, year: 2000, power: 50, location: 'Akihabara')
-
-boat.create(name: 'Pigeon', price: 10000, owner: 'Hessa', capacity: 3, year: 2018, power: 3000, location: 'Tsukihji')
-
